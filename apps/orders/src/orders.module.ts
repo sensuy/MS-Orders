dc4fefd2875cbb4c89c92a59a973b99517d96c34
Sensui <sensuijunior@hotmail.com>
2023-04-16 12:43:43 -0300

    feature: orders configured

--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
+import * as Joi from 'joi';
+import { ConfigModule } from '@nestjs/config';
+
+import { DatabaseModule } from '@app/common';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
+import { OrderRepository } from './orders.repository';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Order, OrderSchema } from './schemas/order.schema';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGO_URI: Joi.string().required(),
+      }),
+      envFilePath: './apps/orders/.env',
+    }),
+    DatabaseModule,
+    MongooseModule.forFeature([{ name: Order.name, schema: OrderSchema }]),
+
+  ],
   controllers: [OrdersController],
-  providers: [OrdersService],
+  providers: [OrdersService, OrderRepository],
 })
-export class OrdersModule {}
+export class OrdersModule { }
---
+++ apps/orders/src/orders.repository.ts
@@ -0,0 +1,18 @@
+import { Injectable, Logger } from "@nestjs/common";
+import { InjectConnection, InjectModel } from "@nestjs/mongoose";
+import { Model,  Connection } from "mongoose";
+import { AbstractRepository } from "@app/common";
+import { Order } from "./schemas/order.schema";
+
+@Injectable()
+export class OrderRepository extends AbstractRepository<Order> {
+
+  protected readonly logger = new Logger(OrderRepository.name);
+
+  constructor(
+    @InjectModel(Order.name)  OrderModel: Model<Order>,
+    @InjectConnection() connection: Connection
+  ) {
+    super(OrderModel, connection);
+  }
+}
