69dc54054d249edbc89feb42bec21abe80d9210b
Sensui <sensuijunior@hotmail.com>
2023-04-20 05:32:25 -0300

    fix: order observable lastValueFrom

--- apps/orders/src/orders.service.ts
+++ apps/orders/src/orders.service.ts
@@ -3,16 +3,26 @@ import { OrderRepository } from './orders.repository';
 import { CreateOrderRequest } from './dto/create-order.request';
 import { BILLING_SERVICE } from './constants/service';
 import { ClientProxy } from '@nestjs/microservices';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable()
 export class OrdersService {
   constructor(
     private readonly ordersRepository: OrderRepository,
-    @Inject(BILLING_SERVICE) private readonly billingService: ClientProxy
+    @Inject(BILLING_SERVICE) private readonly billingClient: ClientProxy
   ) { }
-
-  createOrder(request: CreateOrderRequest) {
-    return this.ordersRepository.create(request);
+  async createOrder(request: CreateOrderRequest) {
+    const session = await this.ordersRepository.startTransaction();
+    try {
+      const order = await this.ordersRepository.create(request, { session });
+      await lastValueFrom(this.billingClient.send('order_created', { request }), { defaultValue: null });
+      await session.commitTransaction();
+      return order;
+    }
+    catch (error) {
+      await session.abortTransaction();
+      throw error;
+    }
   }
 
   getOrders() {
