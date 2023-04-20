966d420c54fe9fab2d2787d0389ddb2e677aa320
Sensui <sensuijunior@hotmail.com>
2023-04-20 03:59:19 -0300

    feature: billing queue configured

--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import * as Joi from 'joi';
 import { ConfigModule } from '@nestjs/config';
 
-import { DatabaseModule } from '@app/common';
+import { DatabaseModule, RmqModule } from '@app/common';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
 import { OrderRepository } from './orders.repository';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Order, OrderSchema } from './schemas/order.schema';
+import { BILLING_SERVICE } from './constants/service';
 
 @Module({
   imports: [
@@ -21,6 +22,7 @@ import { Order, OrderSchema } from './schemas/order.schema';
     }), 
     DatabaseModule,
     MongooseModule.forFeature([{ name: Order.name, schema: OrderSchema }]),
+    RmqModule.register({ name: BILLING_SERVICE }),
 
   ],
   controllers: [OrdersController],
--- apps/orders/src/orders.service.ts
+++ apps/orders/src/orders.service.ts
@@ -1,12 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { OrderRepository } from './orders.repository';
 import { CreateOrderRequest } from './dto/create-order.request';
+import { BILLING_SERVICE } from './constants/service';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
 export class OrdersService {
- constructor(private readonly ordersRepository: OrderRepository) {}
+  constructor(
+    private readonly ordersRepository: OrderRepository,
+    @Inject(BILLING_SERVICE) private readonly billingService: ClientProxy
+  ) { }
 
- createOrder(request: CreateOrderRequest) {
+  createOrder(request: CreateOrderRequest) {
     return this.ordersRepository.create(request);
   }
 
